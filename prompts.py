320f016b6ad9f3e8548bc6ae4a65fbf40841c8ce
Samuel Simplicio <samuelsimplicio5@gmail.com>
2023-12-24 19:46:43 -0300

    Changed hotel name

--- prompts.py
+++ prompts.py
@@ -3,7 +3,7 @@ from typing import Tuple
 
 def create_prompt_custom(question: str, context: Tuple[str, str]) -> str:
     return f"""
-    You are at Miramar Hotel. Answer the question choosing one of the following data. Discard the data which you didn't use: 
+    You are at Meowmat Hotel. Answer the question choosing one of the following data. Discard the data which you didn't use: 
     ### DATA 1: {context[0]}
     ### DATA 2: {context[1]}
     ### QUESTION: {question}
@@ -14,7 +14,7 @@ def create_prompt_custom(question: str, context: Tuple[str, str]) -> str:
 def create_prompt_alpaca(question: str, context: Tuple[str, str]) -> str:
     data = ''.join(context)
     return f"""
-    You are at Miramar Hotel. Below is an instruction that describes a task. Write a short response that appropriately completes the request based on the following information: {data}
+    You are at Meowmat Hotel. Below is an instruction that describes a task. Write a short response that appropriately completes the request based on the following information: {data}
     ### QUESTION: {question}
     ### RESPONSE:
     """
@@ -22,6 +22,6 @@ def create_prompt_alpaca(question: str, context: Tuple[str, str]) -> str:
 def create_prompt_gpt4(question: str, context: Tuple[str, str]) -> str:
     data = ''.join(context)
     return f"""
-    You are at Miramar Hotel. Below is an instruction that describes a task. Write a short response that appropriately completes the request based on the following information: {data}
+    You are at Meowmat Hotel. Below is an instruction that describes a task. Write a short response that appropriately completes the request based on the following information: {data}
     User: {question}<|end_of_turn|>GPT4 Assistant:
     """
